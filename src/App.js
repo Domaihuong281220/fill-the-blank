30385616316af376aae55ae3dba65d7108f5905c
Domaihuong281220 <80810923+Domaihuong281220@users.noreply.github.com>
2024-11-08 20:33:09 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 // src/App.js
 import React from "react";
 import DragAndDrop from "./DragAndDrop";
+import AnimatedCursor from "react-animated-cursor"
 
 function App() {
   return (
     <div className="App">
+      <AnimatedCursor
+        innerSize={8}
+        outerSize={35}
+        innerScale={1}
+        outerScale={2}
+        outerAlpha={0}
+        showSystemCursor={false}
+        hasBlendMode={true}
+        outerStyle={{
+          border: '3px solid var(--cursor-color)'
+        }}
+        innerStyle={{
+          backgroundColor: 'var(--cursor-color)'
+        }}
+      />
       <DragAndDrop />
     </div>
   );
